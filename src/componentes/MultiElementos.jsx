231d72ac0fdd0e5e496a92fea7b4526ae980f116
Rafael <djalmasantos839@gmail.com>
2019-11-07 11:08:23 -0300

    aulas acrescentadas

---
+++ src/componentes/MultiElementos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => [
+    <h1>Parte 1</h1>,
+    <h2>Parte</h2>
+]
+  
+   
+
+
+//nesse caso é necessario da div, para encapsular o <H1></H1> e <H2></H2>
+//outro metado é usar React.fragment
+// terceiro é usar como se fosse um arraw
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import ReactDOM from 'react-dom';
-import React from 'react'
-
+import React from 'react';
+//import PrimeiroComponenmente from './componentes/PrimeiroComponmente'
+import {CompA, CompB as B} from './componentes/DoisComponente'
+import MultiElementos from './componentes/MultiElementos'
 const elemente = document.getElementById('root')
-const jsx = <h1>Olá react!</h1>
+ReactDOM.render(
+    <div>
+        {/*<MultiElementos/>*/}
+        <CompA valor="Olá, eu sou A"></CompA>
+        <B valor="B na área"></B>
+        {/*<PrimeiroComponenmente valor= 'Bom dia'/>*/}
+    </div>
+, elemente)
+
+
+
+/*
 ReactDOM.render(jsx, elemente)
+const jsx = <h1>Olá React!</h1>
+*/
