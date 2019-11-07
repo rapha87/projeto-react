26b5372079b10880a5b135b2143cba1b4c89c884
Rafael <djalmasantos839@gmail.com>
2019-11-07 20:51:27 -0300

    novas atualizações

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 
 const elemente = document.getElementById('root')
-const jsx = <h1>Olá react!</h1>
+const jsx = <h1>Olá React!!</h1>
 ReactDOM.render(jsx, elemente)
